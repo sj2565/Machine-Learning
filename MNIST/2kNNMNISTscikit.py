259a32f9c67ac545ccc8e19f3cd19f0d3bce5e72
sj2565 <seojoon2565@gmail.com>
2025-01-11 04:45:30 +0900

    AI 학습

---
+++ MNIST/2kNNMNISTscikit.py
@@ -0,0 +1,25 @@
+from sklearn.neighbors import KNeighborsClassifier
+import functions as fs
+import numpy as np
+import time
+
+t1=time.time()
+train,test=fs.init_data() # read data file:train.bin, test.bin
+
+
+trainSet,testSet=fs.data_ready2(train,test) # 일부분의  data 가져오기
+label=np.tile(np.arange(0,10),(300,1))
+
+knn=KNeighborsClassifier(n_neighbors=10,weights="distance",metric="euclidean")
+#knn=KNeighborsClassifier(n_neighbors=10)
+knn.fit(trainSet,label.T.flatten()) # T : transpose
+result=knn.predict(testSet)
+result=result.reshape(10,100).T
+
+
+acc,pre,rec,f1=fs.calcMeasure(result) # 성능지표 계산, acc.sum()해볼것
+t2=time.time()
+print(acc,pre,rec,f1) # class 별 성능
+print(t2-t1)
+
+
