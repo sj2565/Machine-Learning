4053b97127fb26d491c2380f686878aba47c678b
sj2565 <seojoon2565@gmail.com>
2025-01-13 23:57:29 +0900

    AI 학습

---
+++ MNIST/4kNNMNISTLDA.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+import pdb
+import time
+
+import functions as fs 
+
+
+t1=time.time()
+train,test=fs.init_data() # read data file:train.bin, test.bin
+
+##print(len(train))
+##print(len(train[0]))
+##print(train[0][0].shape)
+##
+##for i in range(10):
+##    plt.subplot(2,5,i+1),plt.imshow(train[i][0],'gray')
+##    plt.axis('off')
+##    print(len(train[i]))
+##
+##plt.show()
+
+trainSet,testSet=fs.data_ready2(train,test) # 일부분의  data 가져오기
+trainSetf2, testSetf2=fs.lda(trainSet,testSet,8)
+
+result=fs.knn(trainSetf2,testSetf2,k=500)
+
+acc,pre,rec,f1=fs.calcMeasure(result) # 성능지표 계산
+t2=time.time()
+print(acc,pre,rec,f1) # class 별 성능
+print(t2-t1)
+print('Acc=',acc.mean())
+print('F1=',f1.mean())
